98045497f1000781bee2f451e3aae186f0247086
LucasKhalil <lucaskhalilazevedo20@gmail.com>
2023-09-24 10:44:03 -0300

    new Stack

--- Algoritmos/Stack.java
+++ Algoritmos/Stack.java
@@ -1,135 +1,54 @@
 package Algoritmos;
 
-import javax.management.RuntimeErrorException;
+public class Stack<T> {
 
-public class Stack {
+    T[] content;
+    int top;
+    int capacity;
 
-    private int top;
-    private int[] content;
-
-    // Cria uma pilha vazia com tamanho definido
-    public Stack(int size) {
-        this.top = -1;
-        this.content = new int[size];
+    public Stack(int capacity) {
+        this.content = (T[]) new Comparable[capacity];
+        this.top = 0;
+        this.capacity = capacity;
     }
 
-    // cria uma pilha já com elementos
-    public Stack(int[] content) {
-        this.top = content.length - 1;
-        this.content = content;
+    public void push(T element) {
+        if (this.isFull())
+            throw new RuntimeException();
+
+        this.content[this.top] = element;
+        top++;
     }
 
-    public boolean isEmpty() {
-        return this.top == -1;
+    public T peek() {
+        if (this.isEmpty())
+            throw new RuntimeException();
+
+        return this.content[this.top - 1];
+    }
+
+    public T pop() {
+        if (this.isEmpty())
+            throw new RuntimeException();
+
+        this.top--;
+        return this.content[this.top];
     }
 
     public boolean isFull() {
-        return this.top == this.content.length - 1;
+        return this.top == this.capacity;
     }
 
-    // remove o valor no topo da pilha
-    public int pop() {
-        if (this.isEmpty()) {
-            throw new RuntimeException("Empty Stack!!!");
-        }
-        return this.content[top--];
+    public boolean isEmpty() {
+        return this.top == 0;
     }
 
-    // retorna o valor no topo da pilha
-    public int peek() {
-        if (this.isEmpty()) {
-            throw new RuntimeException("Empty Stack!!!");
-        }
-        return this.content[top];
-    }
-
-    // adiciona um valor à pilha
-    public void push(int n) {
-        if (this.isFull()) {
-            throw new RuntimeException("Stack Overflow!!!");
-        }
-        this.top++;
-        this.content[top] = n;
-    }
-
-    // retorna o tamanho atual da pilha ( sem considerar os espaços vazios )
-    public int size() {
-        return this.top + 1;
-    }
-
-    // retorna a capacidade da pilha ( considerando os espaços vazios )
     public int capacity() {
-        return this.content.length;
+        return this.capacity;
     }
 
-    // verifica se a pilha contém o elemento
-    public boolean contains(int n) { // << com problemas
-        if (this.isEmpty()) {
-            return false;
-        }
-        Stack aux = new Stack(top + 1);
-        while (true) {
-            System.out.println(aux.toString());
-            if (this.isEmpty()) {
-                System.out.println("entrou no isEmpty");
-                break;
-            } else if (this.content[this.top] == n) {
-                System.out.println("Entrou no ==");
-                aux.push(this.pop());
-                break;
-            }
-            aux.push(this.pop());
-        }
-        boolean toReturn = false;
-
-        while (!aux.isEmpty()) {
-            if (aux.peek() == n) {
-                toReturn = true;
-            }
-            this.push(aux.pop());
-        }
-        return toReturn;
-    }
-
-    // retorna o index do elemento na pilha, se ele não estiver presente retorna -1
-    public int indexOf(int n) {
-        if (this.isEmpty()) {
-            return -1;
-        }
-        Stack aux = new Stack(top + 1);
-        while (true) {
-            if (this.isEmpty()) {
-                break;
-            }
-            if (this.content[this.top] == n) {
-                aux.push(this.pop());
-                break;
-            }
-            aux.push(this.pop());
-        }
-        int toReturn = -1;
-        if (aux.peek() == n) {
-            toReturn = this.getTop() + 1;
-        }
-        while (!aux.isEmpty()) {
-            this.push(aux.pop());
-        }
-        return toReturn;
-    }
-
-    public int getTop() {
+    public int size() {
         return this.top;
     }
 
-    public int[] getContent() {
-        return this.content;
-    }
-
-    public String toString() {
-        String toReturn = "";
-        for (int i = this.content.length - 1; i >= 0; i--) {
-            toReturn += "[" + this.content[i] + "]";
-        }
-        return toReturn;
-    }
 }
